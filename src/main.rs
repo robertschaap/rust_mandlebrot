c1ed5bb973e565b65e8b4cb21f68d7a50ed95ca7
robertschaap <robert.schaap@mac.com>
2018-08-27 22:03:32 +0200

    pixel to point function added

--- src/main.rs
+++ src/main.rs
@@ -37,4 +37,19 @@ fn main() {
             None => None
         }
     }
+
+    fn pixel_to_point(bounds: (usize, usize),
+                      pixel: (usize, usize),
+                      upper_left: Complex<f64>,
+                      lower_right: Complex<f64>)
+        -> Complex<f64>
+    {
+        let (width, height) = (lower_right.re - upper_left.re,
+                               upper_left.im - lower_right.im);
+
+        Complex {
+            re: upper_left.re + pixel.0 as f64 * width / bounds.0 as f64,
+            im: upper_left.im + pixel.1 as f64 * width / bounds.1 as f64
+        }
+    }
 }
